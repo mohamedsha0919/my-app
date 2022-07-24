6c764f81260c20b5149d57a55c6d71012ce1ef75
Mohamed Sharif A <mohamedsharifghost@gmail.com>
2022-07-24 14:39:40 +0530

    change

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(){
 	return(
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#home"><img src={logo} className="logo"/></a>
+    <a class="navbar-brand" href="#home"><img src={logo} className="logo" alt=''/></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
